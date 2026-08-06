582d28d9d235da3085e49c570f8f1bbb7bedc68f
tanel-bl <tanel@blacklabz.com>
2026-08-06 15:51:27 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bl-summer-days-v7';
+const CACHE_NAME = 'bl-summer-days-v8';
 const PRECACHE = ['/', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', (event) => {
